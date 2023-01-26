5ff45646111a809046ce9b256f77b5b21ba1d73e
fishwdev <fishwdev@gmail.com>
2023-01-26 22:50:11 +0800

    Add base entity and change id type

--- pet-clinic-data/src/main/java/springfranework/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/springfranework/springpetclinic/services/PetService.java
@@ -5,7 +5,7 @@ import springfranework.springpetclinic.model.Pet;
 import java.util.Set;
 
 public interface PetService {
-    Pet findById(long id);
+    Pet findById(Long id);
 
     Pet save(Pet pet);
 
