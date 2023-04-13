dd37373b6d6ad156812b8e7529f982f4effc783f
fishwdev <fishwdev@gmail.com>
2023-04-13 22:11:42 +0800

    added custom date editor in Visit Controller

--- pet-clinic-web/src/main/java/springframework/springpetclinic/controllers/VisitController.java
+++ pet-clinic-web/src/main/java/springframework/springpetclinic/controllers/VisitController.java
@@ -12,6 +12,8 @@ import springframework.springpetclinic.services.OwnerService;
 import springframework.springpetclinic.services.PetService;
 import springframework.springpetclinic.services.VisitService;
 
+import java.beans.PropertyEditorSupport;
+import java.time.LocalDate;
 import java.util.Map;
 
 @Controller
@@ -28,8 +30,15 @@ class VisitController {
     }
 
     @InitBinder
-    public void setAllowedFields(WebDataBinder dataBinder) {
+    public void dataBinder(WebDataBinder dataBinder) {
         dataBinder.setDisallowedFields("id");
+
+        dataBinder.registerCustomEditor(LocalDate.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String text) throws IllegalArgumentException {
+                setValue(LocalDate.parse(text));
+            }
+        });
     }
 
     /**
